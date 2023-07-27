98195bf82b6632c60cdc3ae2047089a7cb411b30
tosecformback <sub7503@naver.com>
2023-07-27 19:17:21 +0900

    DRF Project Backend

--- account/urls.py
+++ account/urls.py
@@ -7,8 +7,13 @@ urlpatterns = [
     # account/
     path('', views.Index.as_view(), name='logIndex'),
     # 로그인
+    path('login/', views.Login.as_view(), name='login'),
     # 회원가입
+    path('signup/', views.Signup.as_view(), name='signup'),
     # 로그아웃
+    path('logout/', views.Logout.as_view(), name='logout'),
     # 프로필보기
+    path('profile/<str:pk>/', views.ProfileDetail.as_view(), name='profile'),
     # 프로필수정
+    path('profile/<str:pk>/update/', views.ProfileUpdate.as_view(), name='profileUp'),
 ]
